87bc25b302a3a6098891a50feb72a96637d1765b
Rupesh dev <rupeshdev116@gmail.com>
2022-01-17 19:39:10 +0530

    new

---
+++ digitalocean/k8s/var.tf
@@ -0,0 +1,12 @@
+variable "NAME" {
+    default = "multi-k8s"
+}
+variable "REGION" {
+  default = "blr1"
+}
+variable "IMAGE"{
+    default ="s-2vcpu-2gb"
+}
+variable "K8S_VERSION" {
+    default = "1.21.5-do.0"
+}
